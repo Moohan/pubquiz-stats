765fd82d537d2b992807cdf4a44229c98467704d
James McMahon <jamesmcmahon0@gmail.com>
2021-04-24 19:07:59 +0100

    Use Oauth to enable write access

--- code/import_data.R
+++ code/import_data.R
@@ -1,9 +1,6 @@
-sheet_url <- "https://docs.google.com/spreadsheets/d/12yUhkqq5ajM_uq7vLytH9cBceCCDFC0MEJluVX_aBdA/edit?usp=sharing"
-
-# Sheet doesn't need auth as it has link editing enabled
-gs4_deauth()
+sheet_id <- "12yUhkqq5ajM_uq7vLytH9cBceCCDFC0MEJluVX_aBdA"
 
 # Read the sheet and clean up the variable names
 quiz_data <-
-  read_sheet(sheet_url) %>%
+  read_sheet("12yUhkqq5ajM_uq7vLytH9cBceCCDFC0MEJluVX_aBdA") %>%
   clean_names()
